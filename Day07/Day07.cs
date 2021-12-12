048664d4bd06f64d210a7440edcbdad2dbaaaf0b
Matevž Sešel <matevz.sesel@gmail.com>
2021-12-12 20:53:03 +0100

    day 7

--- Day07/Day07.cs
+++ Day07/Day07.cs
@@ -9,7 +9,53 @@ namespace Day07
 {
     public class Day07 : Solver
     {
-        public override string PartOne() => "Not Solved.";
-        public override string PartTwo() => "Not Solved.";
+        private record CrabRate(int Position, int Count);
+        private readonly Dictionary<int, int> _input;
+
+        public Day07()
+        {
+            _input = ReadInputText<string>()
+                .Split(",")
+                .Select(int.Parse)
+                .GroupBy(x => x)
+                .Select(x => new CrabRate(x.Key, x.Count(y => y == x.Key)))
+                .ToDictionary(x => x.Position, x => x.Count);
+        }
+
+        public override string PartOne()
+        {
+            var minFuel = int.MaxValue;
+
+            foreach (var pos in _input)
+            {
+                var fuel = 0;
+                foreach (var comp in _input)
+                    fuel += comp.Value * Math.Abs(comp.Key - pos.Key);
+
+                if (fuel < minFuel)
+                    minFuel = fuel;
+            }
+            return minFuel.ToString();
+        }
+
+        public override string PartTwo()
+        {
+            var positionRange = Enumerable.Range(1, _input.Count).ToArray();
+            var minFuel = long.MaxValue;
+
+            foreach (var i in positionRange)
+            {
+                var fuel = 0;
+                foreach (var comp in _input)
+                {
+                    var diff = Math.Abs(i - comp.Key);
+                    fuel += diff * (diff + 1) / 2 * comp.Value;
+                }
+
+                if (fuel < minFuel)
+                    minFuel = fuel;
+            }
+            return minFuel.ToString();
+        }
     }
 }
